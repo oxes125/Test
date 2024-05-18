5f5e9435006241d7aa121d4303b52297c5671272
oxes125 <oxes125@hotmail.com>
2024-05-17 19:22:20 -0600

    saeb

--- algo.js
+++ algo.js
@@ -3,11 +3,11 @@ console.log();git <div class="dropdown open">sdfdfs
     <sdf
         class="btn btn-secondary dropdown-toggle"sdf
         type="button"fsdf
-        id="sdf"fds
+        id=rwer"sdf"fds
         data-bs-toggle="dfsdropdown"fds
         aria-sdf="true"fsd
-        aria-expanded=sdffalse"sdfsd
-    >sdf
+        erew-expanded=sdffalse"sdfsd
+    >wer
 sdf    </a>
     <div class="dropdown-menu" aria-labelledby="triggerId">
         <a class="dropdown-item" href="#">Action</a>
