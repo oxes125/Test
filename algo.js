feb91f131181bf5818d6b60e29f14014f3c1358d
oxes125 <oxes125@hotmail.com>
2024-05-17 19:15:28 -0600

    saeb

--- algo.js
+++ algo.js
@@ -1,14 +1,14 @@
 
-console.log();git <div class="dropdown open">
-    <a
-        class="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="triggerId"
-        data-bs-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false"
-    >
-        Dropdown Anchor
+console.log();git <div class="dropdown open">sdfdfs
+    <asdf
+        class="btn btn-secondary dropdown-toggle"sdf
+        type="button"fsdf
+        id="triggerId"fds
+        data-bs-toggle="dfsdropdown"fds
+        aria-haspopup="true"fsd
+        aria-expanded="false"sdfsd
+    >sdf
+        Dropdown Anchorsdf
     </a>
     <div class="dropdown-menu" aria-labelledby="triggerId">
         <a class="dropdown-item" href="#">Action</a>
